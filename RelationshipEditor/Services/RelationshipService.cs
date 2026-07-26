83ab3d3bc6b89da2549098e9ca936cb510a0a4e3
ItzMady <madeline.rahir@gmail.com>
2026-07-26 12:07:15 +0200

    Update RelationshipService.cs

--- RelationshipEditor/Services/RelationshipService.cs
+++ RelationshipEditor/Services/RelationshipService.cs
@@ -1,4 +1,6 @@
-﻿using StardewModdingAPI;
+using System;
+using System.Linq;
+using StardewModdingAPI;
 using StardewValley;
 
 namespace RelationshipEditor.Services;
@@ -14,7 +16,10 @@ internal class RelationshipService
 
     public bool SetHearts(string npcName, int hearts)
     {
-        NPC? npc = Game1.getCharacterFromName(npcName);
+        NPC? npc = Utility.getAllCharacters()
+            .Where(n => n.CanSocialize)
+            .FirstOrDefault(n =>
+                n.Name.Equals(npcName, StringComparison.OrdinalIgnoreCase));
 
         if (npc is null)
         {
@@ -30,11 +35,40 @@ internal class RelationshipService
 
         friendship.Points = hearts * 250;
 
-        this.monitor.Log(
-            $"Successfully set {npc.Name}'s friendship to {hearts} hearts.",
-            LogLevel.Info
-        );
+        this.monitor.Log($"Successfully set {npc.Name}'s friendship to {hearts} hearts.", LogLevel.Info);
 
         return true;
+    }
+
+    public void SetAllHearts(int hearts)
+    {
+        int updated = 0;
+
+        foreach (NPC npc in Utility.getAllCharacters()
+                     .Where(n => n.CanSocialize))
+        {
+            if (!Game1.player.friendshipData.TryGetValue(npc.Name, out Friendship? friendship))
+            {
+                friendship = new Friendship();
+                Game1.player.friendshipData.Add(npc.Name, friendship);
+            }
+
+            friendship.Points = hearts * 250;
+            updated++;
+        }
+
+        this.monitor.Log($"Successfully updated {updated} NPCs to {hearts} hearts.", LogLevel.Info);
+    }
+
+    public void ListNPCs()
+    {
+        this.monitor.Log("Available NPCs:", LogLevel.Info);
+
+        foreach (NPC npc in Utility.getAllCharacters()
+                     .Where(n => n.CanSocialize)
+                     .OrderBy(n => n.Name))
+        {
+            this.monitor.Log($"- {npc.Name}", LogLevel.Info);
+        }
     }
 }
