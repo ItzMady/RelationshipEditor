18ed1909337db5a6f79d741c753ad5fa409e9b76
ItzMady <madeline.rahir@gmail.com>
2026-07-26 16:37:43 +0200

    Update ModEntry.cs

--- RelationshipEditor/ModEntry.cs
+++ RelationshipEditor/ModEntry.cs
@@ -7,19 +7,11 @@ public class ModEntry : Mod
 {
     public override void Entry(IModHelper helper)
     {
-        Monitor.Log("Relationship Editor loaded successfully.", LogLevel.Info);
-
-        RegisterCommands(helper);
-    }
-
-    private void RegisterCommands(IModHelper helper)
-    {
-        RelationshipCommand relationshipCommand = new(Monitor);
-
         helper.ConsoleCommands.Add(
             "social",
-            "Usage: relationship <NPC> <hearts>",
-            relationshipCommand.Execute
-        );
+            "Edit NPC friendship.",
+            new RelationshipCommand(Monitor).Execute);
+
+        Monitor.Log("Relationship Editor loaded.", LogLevel.Info);
     }
 }
